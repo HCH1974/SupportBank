ae944ec88eebecbeb3907c9106dcadddc0b87151
Hannah Hukins <hannah.hukins@gmail.com>
2023-02-22 15:54:26 +0000

    refactored into methods

--- Bank.cs
+++ Bank.cs
@@ -8,8 +8,32 @@ class Bank
     {
         AccountList = new List<Account>();
     }
+    public void ProcessInputFile(string inputFileName)
+    {
+        var Lines = File.ReadLines(inputFileName);
+        List<string> transactions = new List<string>();
 
-    public Account FindOrCreateAccount(string accountname)
+        foreach (string line in Lines)
+        {
+            transactions.Add(line);
+        }
+        // First line has column headings
+        transactions.RemoveAt(0);
+
+        for (int i = 0; i < transactions.Count; i++)
+        {
+            string[] transactionArr = transactions[i].Split(",");
+
+            if (!CheckDataFormat(transactionArr[0] , transactionArr[4] , i)) continue;
+
+            Account accountFrom = FindOrCreateAccount(transactionArr[1]);
+            Account accountTo = FindOrCreateAccount(transactionArr[2]);
+
+            accountFrom.AddTransactionOut(transactionArr[0], accountFrom, accountTo, transactionArr[3], Decimal.Parse(transactionArr[4]));
+            accountTo.AddTransactionIn(transactionArr[0], accountFrom, accountTo, transactionArr[3], Decimal.Parse(transactionArr[4]));
+        }
+    }   
+   public Account FindOrCreateAccount(string accountname)
     {
         foreach (Account account in AccountList)
         {
@@ -19,19 +43,15 @@ class Bank
         AccountList.Add(newAccount);
         return newAccount;
     }
-
     public void ListAll()
     {
         foreach (Account account in AccountList)
         {
-
-
             decimal amountIn = 0m;
             foreach (Transaction transaction in account.TransactionsIn)
             {
                 amountIn += transaction.Amount;
             }
-
 
             decimal amountOut = 0m;
             foreach (Transaction transaction in account.TransactionsOut)
@@ -40,5 +60,28 @@ class Bank
             }
             Console.WriteLine($"Account: {account.Name}\n\tAmount owing: £{amountIn}\n\tAmount owed: £{amountOut}");
         }
+    }
+
+     public bool CheckDataFormat(string dateString , string amountString, int i)
+     {
+        try
+        {
+            DateTime.Parse(dateString);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine($"Invalid date format on line {i + 2}: '{dateString}'. Date must be in dd/mm/yy.");
+            return false;
+        }
+        try
+        {
+            Decimal.Parse(amountString);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine($"Invalid amount on line {i + 2}: '{amountString}'. Amount must be in £x.xx format.");
+            return false;
+        }
+        return true;
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,49 +2,10 @@
 
 Bank ourBank = new Bank();
 
-var Lines = File.ReadLines("DodgyTransactions2015.csv");
+Console.Write("Enter the name of your input data file: ");
+string inputFile = Console.ReadLine()!;
 
-List<string> transactions = new List<string>();
-
-foreach (string line in Lines)
-{
-    transactions.Add(line);
-}
-
-transactions.RemoveAt(0);
-
-for (int i = 0; i < transactions.Count; i++)
-{
-    string[] transactionArr = transactions[i].Split(",");
-
-    try
-    {
-        DateTime.Parse(transactionArr[0]);
-    }
-    catch (FormatException)
-    {
-        Console.WriteLine($"Invalid date format on line {i + 2}: '{transactionArr[0]}'. Date must be in dd/mm/yy.");
-        continue;
-    }
-
-    try
-    {
-        Decimal.Parse(transactionArr[4]);
-    }
-    catch (FormatException)
-    {
-        Console.WriteLine($"Invalid amount on line {i + 2}: '{transactionArr[4]}'. Amount must be in £x.xx format.");
-        continue;
-    }
-
-    Account accountFrom = ourBank.FindOrCreateAccount(transactionArr[1]);
-
-    Account accountTo = ourBank.FindOrCreateAccount(transactionArr[2]);
-
-    accountFrom.AddTransactionOut(transactionArr[0], accountFrom, accountTo, transactionArr[3], Decimal.Parse(transactionArr[4]));
-
-    accountTo.AddTransactionIn(transactionArr[0], accountFrom, accountTo, transactionArr[3], Decimal.Parse(transactionArr[4]));
-}
+ourBank.ProcessInputFile(inputFile);
 
 Console.Write("Would you like to (1) ListAll  or (2) List[Account]: ");
 string choice = Console.ReadLine()!;
@@ -53,10 +14,10 @@ if (choice == "1")
 {
     ourBank.ListAll();
 }
-
-if (choice == "2")
+else if (choice == "2")
 {
     Console.Write("Please enter account name: ");
     string accChoice = Console.ReadLine()!;
+    
     ourBank.FindOrCreateAccount(accChoice).ListAccount();
 }
